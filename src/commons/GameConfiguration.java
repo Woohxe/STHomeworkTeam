af37fdf79296cd3f946ba4d5c26037654f09def5
lero1005 <50492628+lero1005@users.noreply.github.com>
2020-07-03 02:36:12 +0800

    添加注释

--- src/commons/GameConfiguration.java
+++ src/commons/GameConfiguration.java
@@ -2,33 +2,56 @@ package commons;
 
 
 import GUI.GamePanel1;
-import GUI.MainFrame;
 
 /**
+ * 此类为游戏配置，定义了游戏的一些基本参数。
  * Created by lero on 2020/6/30.
  */
 public class GameConfiguration {
+
+    //baseLength规定了游戏界面中每一格的长度
     private int baseLength = 150;
+    //定义了用户鼠标触发监听器的拖动长度
     private int LIMIT = 150;
+    //定义游戏模式
     private int GameMode = 1;
+    //定义游戏界面的行数
     private int rowNum = 5;
+    //定义游戏界面的列数
     private int columnNum = 4;
 
+    //定义游戏状态是否为自动寻路
     private boolean aoto = false;
+    //定义是否计时
     private boolean timing = false;
+    //
     private int mapStep[][];//二维数组
+    //定义游戏中的地图标号
     private int gameId;
 
+    /**
+     * 构造方法，传入游戏界面1，获取界面中用户选定的地图标号
+     * 调用initMap()方法，初始化地图
+     * @param gamePanel1
+     */
     public GameConfiguration(GamePanel1 gamePanel1) {
         gameId = gamePanel1.getChooseNum();
         initMap();
 
     }
 
+    /**
+     * 初始化地图
+     */
     public void initMap() {
         mapStep = GameMap.getMap(gameId);
     }
 
+    /**
+     *
+     * @param baseLength
+     * @param gameMode
+     */
     public GameConfiguration(int baseLength, int gameMode) {
         this.baseLength = baseLength;
         GameMode = gameMode;
