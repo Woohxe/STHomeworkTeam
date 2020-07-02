5f9bfcff1eddbe4fab25962de3211ed8606e84dc
lero1005 <50492628+lero1005@users.noreply.github.com>
2020-07-03 02:40:04 +0800

    添加注释

--- src/utils/TimeRecordReader.java
+++ src/utils/TimeRecordReader.java
@@ -9,26 +9,45 @@ import java.text.ParseException;
 import java.util.List;
 
 /**
+ * 读取关卡排行榜的工具类
  * Created by lero on 2020/7/2.
  */
 public class TimeRecordReader {
+
+    //记录关卡排行榜的文件名
     private String filename;
+    //对应文件下的所有记录
     private TimeModeRecord[] records;
 
+    /**
+     * 根据关卡编号获取文件，并将所有记录存入records数组
+     * @param mapId
+     * @throws Exception
+     * @throws ParseException
+     */
     public TimeRecordReader(int mapId) throws Exception,ParseException {
+
+        //记录的文件名
         filename = "time_mode_xml/"+String.valueOf(mapId) + "_time_mode.xml";
         System.out.println(filename);
+        //SAX解析器用于解析xml文件
         SAXBuilder builder = new SAXBuilder();
 
+        //读入对应的文件，存在document中
         Document document = builder.build(filename);
 
+        //获取xml中的根节点
         Element root = document.getRootElement();
 
+        //以列表的形式获取记录
         List list = root.getChildren("record");
+
+        //构建相应长度的数组
         records = new TimeModeRecord[list.size()];
 
 
-
+        //根据xml文件中每条记录的属性值构建TimeModeRecord对象
+        //写入records数组
         for (int i = 0; i<list.size(); i++) {
             Element record = (Element) list.get(i);
             String name = record.getChildText("name");
--- src/utils/TimeRecordWriter.java
+++ src/utils/TimeRecordWriter.java
@@ -10,25 +10,44 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
+ * 写入排行榜文件的工具类
  * Created by lero on 2020/7/2.
  */
 public class TimeRecordWriter {
+
+    //待写入文件的记录
     private TimeModeRecord[] records;
 
+    /**
+     * 构建函数，参数为待写入的记录数组。
+     * @param records
+     */
     public TimeRecordWriter(TimeModeRecord[] records) {
         this.records = records;
     }
 
+    /**
+     * 将记录写入对应的xml文件
+     * @throws IOException
+     */
     public void write() throws IOException {
+
+        //如果记录数组为空，则无需再去写入
         if(records.length == 0)
             return;
+        //根据记录中的关卡编号，得到待重写的xml文件名
         String filename = String.valueOf(records[0].getMapId());
         filename = "time_mode_xml/"+filename + "_time_mode.xml";
 
+        //将待写入的数据存入document文件中
         Document document = new Document();
+        //xml文件的根节点为list
         Element list = new Element("list");
+        //list元素加入到document下
         document.addContent(list);
 
+        //遍历待写入文件的记录数组，根据每条记录的值，加入到record节点下
+        //作为list的子结点加入
         for(int i = 0; i<records.length; i++) {
             //System.out.println(records[i]);
 
@@ -50,10 +69,12 @@ public class TimeRecordWriter {
             record.addContent(date);
         }
 
+        //设置写入的格式，编码和缩进
         Format format = Format.getPrettyFormat();
         format.setEncoding("utf-8");
         format.setIndent("    ");
 
+        //写入xml文件
         XMLOutputter out = new XMLOutputter(format);
         out.output(document, new FileOutputStream(filename));
     }
